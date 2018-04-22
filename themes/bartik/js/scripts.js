4b2639130e85f2084128706b644c27dd95e21c99
Vladimir Degtiarev <vladimir@ownstr.com>
2018-04-22 08:17:05 +0800

    JS changes

--- themes/bartik/js/scripts.js
+++ themes/bartik/js/scripts.js
@@ -2,14 +2,14 @@
     'use strict';
     Drupal.behaviors.global = {
         attach: function (context,settings) {
-
+            console.log('HI!');
             //Service page - right menu
             if ($('body ul.menu-right').length) {
                 $('body').once('rightMenu').each(function() {
                     //var menuItem = $('body ul.menu-right li');
                     var contentH2 = $('body ul.menu-right li');
                     //$(menuItem).hide();
-
+                    console.log('Start');
                     if ($(contentH2).length) {
                         $(contentH2).each(function (index, element) {
                             // index (число) - текущий индекс итерации (цикла)
