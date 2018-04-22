b7021f9ee226f2bcca1ba1291a30a028905f40b8
Vladimir Degtiarev <vladimir@ownstr.com>
2018-04-22 12:09:28 +0800

    JS changes

--- themes/bartik/js/scripts.js
+++ themes/bartik/js/scripts.js
@@ -6,7 +6,7 @@
             //Service page - right menu
             if ($('body ul.menu-right').length) {
                 $('body').once('rightMenu').each(function() {
-                    //var menuItem = $('body ul.menu-right li');
+                    var menuItem = $('body ul.menu-right');
                     var contentH2 = $('body article .node__content h2');
                     //$(menuItem).hide();
 
@@ -19,6 +19,7 @@
                             // element - содержит DOM-ссылку на текущий элемент
 
                             console.log('Индекс элемента h2: ' + index + '; id элемента = ' + $(element).text());
+                            $(menuItem).append('<li class="item"><a href="#"></a>'+$(element).text()+'</a></li>');
                         });
                         //$(menuItem).filter('.item_gallery').show();
                         //$('article .field--name-field-tour-photo-gallery').prepend('<a id="anchor_gallery" class="anchor"></a>');
