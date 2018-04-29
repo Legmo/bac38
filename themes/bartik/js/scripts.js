1a985dbb7722dfb636ac74b815ceb223b3f839bf
Vladimir Degtiarev <vladimir@ownstr.com>
2018-04-29 19:28:29 +0800

    JS changes

--- themes/bartik/js/scripts.js
+++ themes/bartik/js/scripts.js
@@ -33,7 +33,7 @@
 
             //Smooth scroll for right menu on Service page
             $(function(){
-                $("a[href^='#']").click(function(){
+                $("#block-bokovoemenunastranicesospiskomuslug a[href^='#']").click(function(){
                     var _href = $(this).attr("href");
                     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
                     return false;
